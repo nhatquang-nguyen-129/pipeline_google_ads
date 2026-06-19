fcb61a60311ef9fad755c8bb07478feff085fb14
Quang <quang@Quangs-MacBook-Pro.local>
2026-06-19 16:36:06 +0700

    added additional information for input dataframe

--- etl/transform_campaign_metadata.py
+++ etl/transform_campaign_metadata.py
@@ -23,6 +23,7 @@ def transform_campaign_metadata(
             Enforced campaign metadata records
     """
 
+    # Validate input
     print(
         "🔄 [TRANSFORM] Validating column(s) for "
         f"{len(df)} row(s) of Google Ads campaign metadata..."
